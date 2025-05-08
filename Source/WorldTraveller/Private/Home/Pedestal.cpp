5716d32bfe18b0b35238ae6aecea44f7f899410b
foriver4725 <hrt67@icloud.com>
2025-05-08 10:42:46 +0900

    added pedestal model

---
+++ Source/WorldTraveller/Private/Home/Pedestal.cpp
@@ -0,0 +1,20 @@
+﻿#include "Home/Pedestal.h"
+
+APedestal::APedestal()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void APedestal::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void APedestal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/WorldTraveller/Public/Home/Pedestal.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pedestal.generated.h"
+
+UCLASS()
+class WORLDTRAVELLER_API APedestal : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APedestal();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+};
