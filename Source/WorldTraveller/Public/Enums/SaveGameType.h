000e028aebda0477e28fefa68c4d029b26656794
foriver4725 <hrt67@icloud.com>
2025-05-22 16:24:02 +0900

    Add save game management system
    Implement save and load functionality for game states using new classes `UProgSaveGame` and `UItemSaveGame`. Introduce `ASaveGameManager` for managing save operations, including auto-saving. Define `ESaveGameType` enumeration for save type differentiation and update header files accordingly.

---
+++ Source/WorldTraveller/Public/Enums/SaveGameType.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SaveGameType.generated.h"
+
+UENUM()
+enum class ESaveGameType : uint8
+{
+	Prog,
+	Item,
+	All,
+};
---
+++ Source/WorldTraveller/Public/SaveGames/ProgSaveGame.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "ProgSaveGame.generated.h"
+
+UCLASS()
+class WORLDTRAVELLER_API UProgSaveGame final : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	inline bool GetHasFirstLogined() const { return hasFirstLogined; }
+	inline void SetHasFirstLogined(bool b) { hasFirstLogined = b; }
+
+private:
+	UPROPERTY() bool hasFirstLogined = false;
+};
