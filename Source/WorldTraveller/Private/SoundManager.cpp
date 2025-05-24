d2eb202cd14605ce10b717d7ceb0d042220bc424
foriver4725 <hrt67@icloud.com>
2025-05-24 20:40:59 +0900

    Add sound management system and related assets
    - Introduced `BP_SoundManager.uasset` and `SC_Main_GameEnded.uasset`.
    - Deleted `SC_GameEnded.uasset`.
    - Updated multiple binary files including `Home.umap` and `Main.umap`.
    - Implemented `ASoundManager` class with sound playback functionality in `SoundManager.cpp`.
    - Defined `ESoundType` enumeration in `SoundType.h` for sound categorization.
    - Enhanced `SoundManager.h` with properties for sound asset management.

---
+++ Source/WorldTraveller/Private/SoundManager.cpp
@@ -0,0 +1,24 @@
+﻿#include "SoundManager.h"
+#include "Kismet/GameplayStatics.h"
+
+ASoundManager::ASoundManager()
+{
+}
+
+void ASoundManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (instance.IsValid())
+	{
+		this->Destroy();
+		return;
+	}
+	instance = this;
+}
+
+void ASoundManager::Play2D(ESoundType type) const
+{
+	if (USoundBase* sound = sounds[type])
+		UGameplayStatics::PlaySound2D(GetWorld(), sound);
+}
---
+++ Source/WorldTraveller/Public/SoundManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SoundManager.generated.h"
+
+enum class ESoundType : uint8;
+
+UCLASS()
+class WORLDTRAVELLER_API ASoundManager final : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASoundManager();
+
+	static inline ASoundManager* Instance() { return instance.Get(); }
+
+	void Play2D(ESoundType type) const;
+
+protected:
+	virtual void BeginPlay() override final;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Property|Data")
+	TMap<ESoundType, TObjectPtr<USoundBase>> sounds;
+
+	static inline TWeakObjectPtr<ASoundManager> instance = nullptr;
+};
